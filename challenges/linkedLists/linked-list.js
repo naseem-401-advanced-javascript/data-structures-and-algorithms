4f1690714c4177b2a05625dfe85ae5b18e3e6e38
naseem <izzatnaseem30@gmail.com>
2020-01-31 15:22:15 +0200

    fixing the eeslint probs and create a README file with white board

--- challenges/linkedLists/linked-list.js
+++ challenges/linkedLists/linked-list.js
@@ -1,55 +1,55 @@
 'use strict';
+
 class Node {
     constructor(value) {
         this.value = value;
         this.next = null;
-    }
-}
+    };
+};
 
 class LinkedList {
     constructor() {
-        this.head = null
-    }
+        this.head = null;
+    };
 
     insert(value) {
         let node = new Node(value);
         if (!this.head) {
-            this.head = node
-            return this
-        }
+            this.head = node;
+            return this;
+        };
         let currentNode = this.head;
         while (currentNode.next) {
             currentNode = currentNode.next;
-        }
+        };
         currentNode.next = node;
-        return this
-
-    }
+        return this;
+    };
 
     includes(value) {
-        let currentNode = this.head
+        let currentNode = this.head;
         while (currentNode) {
             if (currentNode.value === value) {
                 return true;
             } else {
                 currentNode = currentNode.next;
-            }
-        }
+            };
+        };
         return false;
-    }
+    };
 
     toString() {
         let currentNode = this.head;
-        let linkedListStreng = ''
+        let linkedListStreng = '';
         while (currentNode) {
             linkedListStreng += ` ${currentNode.value} ->`;
             currentNode = currentNode.next;
-        }
+        };
         return linkedListStreng += ` NULL`;
-    }
+    };
 
     insertBefore(value, newVal) {
-        let newNode = new Node(newVal)
+        let newNode = new Node(newVal);
         let currentNode = this.head;
         let preNode = null;
 
@@ -59,39 +59,37 @@ class LinkedList {
                     this.head = newNode;
                 } else {
                     preNode.next = newNode;
-                }
+                };
                 newNode.next = currentNode;
                 break;
-            }
-            preNode = currentNode
-            currentNode = currentNode.next
-        }
-
-    }
+            };
+            preNode = currentNode;
+            currentNode = currentNode.next;
+        };
+    };
 
     insertAfter(value, newVal) {
         let savedNode = null;
-        let newNode = new Node(newVal)
-        let currentNode = this.head
+        let newNode = new Node(newVal);
+        let currentNode = this.head;
 
         while (currentNode) {
             if (currentNode.value === value) {
                 savedNode = currentNode.next
                 currentNode.next = newNode;
                 newNode.next = savedNode;
-            }
+            };
             currentNode = currentNode.next;
-
-        }
-    }
-}
+        };
+    };
+};
 
 const nas = new LinkedList();
 
-nas.insert(1)
-nas.insert(2)
-nas.insert(3)
-nas.insertBefore(2, 1.5)
+nas.insert(1);
+nas.insert(2);
+nas.insert(3);
+nas.insertBefore(2, 1.5);
 console.log(nas.head);
 
 
