dd6ec2fdf75fe13d8d475efd9c3ccde8568910b9
naseem <izzatnaseem30@gmail.com>
2020-02-02 22:06:04 +0200

    complete

---
+++ challenges/queueWithStacks/__tests__/queue-with-stack.test.js
@@ -0,0 +1,31 @@
+'use strict';
+const  PseudoQueue = require('../queue-with-stacks');
+let pseudoQueue = new PseudoQueue();
+
+describe('test the class of pseudoqueue',()=>{
+    beforeEach(()=>{
+        pseudoQueue= new PseudoQueue();
+    });
+    it('It should successfully enqueue items', () => {
+        pseudoQueue.enqueue('10');
+        pseudoQueue.enqueue('15');
+        pseudoQueue.enqueue('20');
+        expect(pseudoQueue.stackFirst).toEqual(['10', '15', '20']);
+    });
+    it('It should successfully dequeue items', () => {
+        pseudoQueue.enqueue('10');
+        pseudoQueue.enqueue('15');
+        pseudoQueue.enqueue('20');
+        pseudoQueue.dequeue();
+        expect(pseudoQueue.stackFirst).toEqual(['10', '15']);
+      });
+      it('It should successfully dequeue all items', () => {
+        pseudoQueue.enqueue('10');
+        pseudoQueue.enqueue('15');
+        pseudoQueue.enqueue('20');
+        pseudoQueue.dequeue();
+        pseudoQueue.dequeue();
+        pseudoQueue.dequeue();
+        expect(pseudoQueue.stackFirst).toEqual([]);    
+      }); 
+})
